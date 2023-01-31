2cf2cde9cfb591f770ecdfbe008de00a23e38223
apurugganan <apurugganan1@my.bcit.ca>
2023-01-30 18:15:37 -0800

    fixed layout for recent projects

---
+++ projects.js
@@ -0,0 +1,22 @@
+const container = document.querySelector(".projects-container");
+
+const card1 = document.createElement("div");
+const card2 = document.createElement("div");
+const card3 = document.createElement("div");
+const card4 = document.createElement("div");
+const card5 = document.createElement("div");
+const card6 = document.createElement("div");
+
+card1.classList.add("project-card");
+card2.classList.add("project-card");
+card3.classList.add("project-card");
+card4.classList.add("project-card");
+card5.classList.add("project-card");
+card6.classList.add("project-card");
+
+container.appendChild(card1);
+container.appendChild(card2);
+container.appendChild(card3);
+container.appendChild(card4);
+container.appendChild(card5);
+container.appendChild(card6);
